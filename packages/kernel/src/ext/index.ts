e7a1715e00a3c866a9f4e47afdc7729756d3b06a
Beef <beefers@riseup.net>
2023-07-15 02:47:23 +0100

    [Packages > Kernel] Add example-ish keygen plugin

--- packages/kernel/src/ext/index.ts
+++ packages/kernel/src/ext/index.ts
@@ -1,10 +1,12 @@
 import unbox from "@ext/unbox";
 import royalmail from "@ext/royalmail";
+import keygen from "@ext/keygen";
 
 // TODO: User extensions
 export const builtInExtensions = [
     unbox,
     royalmail,
+    keygen,
 ];
 
 export const extensions = [...builtInExtensions];
---
+++ packages/kernel/src/ext/keygen.ts
@@ -0,0 +1,17 @@
+export default {
+    patches: [
+        {
+            find: /\(await\(0,.{1,2}\..{1,2}\)\(i\.value\)\)\.valid/,
+            replace: "true",
+        },
+        {
+            find: "the product key you have received on Patreon",
+            replace: (match) => `<s>${match}</s> any product key`, 
+        },
+    ],
+    manifest: {
+        name: "keygen",
+        description: "makes the keymgr app (patreon rewards app) accept any key",
+        authors: ["Beef"],
+    },
+} as Extension;
