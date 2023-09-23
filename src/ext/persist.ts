61d2c0d7e054bd917e26f4b52078dbb943bbe6b9
redstonekasi <kasi@hanneskann.de>
2023-09-23 21:32:36 +0200

    [Ext > persist] Initial commit

---
+++ src/ext/persist.ts
@@ -0,0 +1,25 @@
+import { defineExt } from "@lib/utils"
+
+// TODO: Add a checkbox in the reset wizard
+export default defineExt({
+    patches: [
+        {
+            executable: "C:/system/local/bin/resetwiz",
+            find: /localStorage\.clear\(\)/g,
+            replace: (o) => `$self.save(),${o},$self.restore()`,
+        },
+    ],
+    manifest: {
+        name: "persist",
+        description: "persist ropeswing installation across reset",
+        authors: ["redstonekasi"],
+    },
+    kinject: "",
+    save() {
+        this.kinject = kutil.sysrom.read("KINJECT.js");
+    },
+    restore() {
+        kutil.sysrom.write("KINJECT.js", this.kinject);
+    },
+    core: true,
+});
