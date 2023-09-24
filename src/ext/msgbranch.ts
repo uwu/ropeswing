5f6fbd250afaf477e7dc40fc6430c7a61c00a959
redstonekasi <kasi@hanneskann.de>
2023-09-24 03:40:18 +0200

    [Ext > msgbranch] Initial commit

---
+++ src/ext/msgbranch.ts
@@ -0,0 +1,44 @@
+import { contextify } from "@/core/patcher";
+import { defineExtension } from "@/lib/define";
+import { extensions } from "@ext/all";
+
+// This worker provides a version of MsgRoom that doesn't run the main script while also setting CORS headers.
+const custom = "https://w96-msgroom.kasi.workers.dev";
+
+export default defineExtension({
+	patches: [
+		{
+			executable: "C:/system/local/bin/win96-chat",
+			find: /\((this\.CHAT_URL=.{1,2})\)/,
+			replace: (_, match) => `(${match}="${custom}")`,
+		},
+		{
+			executable: "C:/system/local/bin/win96-chat",
+			find: /,(.{1,2})\.onload=\(\)=>\{/,
+			replace: (_, frame) => `,${frame}.onload=async()=>{await $self.onFrameLoad(${frame}),`,
+		},
+	],
+	manifest: {
+		name: "msgbranch",
+		description: "ropeswing in msgroom",
+		authors: ["redstonekasi"],
+	},
+	async onFrameLoad(frame: HTMLIFrameElement) {
+		const evil = (script: string) => frame.contentWindow!.postMessage({ op: "eval", script }, custom);
+		
+		let main = await fetch(`${custom}/main.js`).then((r) => r.text());
+
+		for (const extension of extensions) {
+			if (!extension.patches) continue;
+
+			for (let patch of extension.patches) {
+				if (patch.executable !== "$msgroom") continue;
+				main = main.replace(patch.find, contextify(patch.replace, extension.manifest.name) as string);
+			}
+		}
+
+		evil(main);
+		evil("app.call(window);");
+	},
+	core: true,
+});
